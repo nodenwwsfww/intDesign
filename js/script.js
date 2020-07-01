a0385c3da8b191d10a203a85c2c1136d903db0b2
Roman Radchenko <nodenwwsfww@gmail.com>
2020-07-01 22:11:24 +0300

    Фикс повторных анимаций попа

--- js/script.js
+++ js/script.js
@@ -79,7 +79,8 @@ window.addEventListener("DOMContentLoaded", () => {
             popUpOpacityValue = 0;
 
         const applyPopUpAnimation = () => {
-            if(popUpOpacityValue >= 1.5 || document.body.clientWidth < 768) {
+            if(popUpOpacityValue >= 1.5) {
+                popUpOpacityValue = 0;
                 return cancelAnimationFrame(popUpAnimID);
             }
             popUpOpacityValue += 0.014;
@@ -87,10 +88,11 @@ window.addEventListener("DOMContentLoaded", () => {
 
             popUpAnimID = requestAnimationFrame(applyPopUpAnimation);
         };
-
         popUpButtonsOpen.forEach(button => button.addEventListener("click", () => {
             popUp.style.display = "block"; 
-            applyPopUpAnimation();
+            if(document.body.clientWidth >= 768) { // ограничение, (от 768 px)
+                applyPopUpAnimation();
+            }
         }));
         popUp.addEventListener("click", event => {
             let target = event.target;
