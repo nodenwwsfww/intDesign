dcf3e0b3ad9edd035bb2fde9490eaa910f6b221a
Roman Radchenko <nodenwwsfww@gmail.com>
2020-07-02 14:10:42 +0300

    slider is ready

--- js/script.js
+++ js/script.js
@@ -146,6 +146,83 @@ window.addEventListener("DOMContentLoaded", () => {
     };
         
     tabsHandler();
+
+    // слайдер
+
+    const slideHandler = () => {
+
+        const slideCollection = document.querySelectorAll(".portfolio-item"),
+            slideButtons = document.querySelectorAll("portfolio-btn"),
+            slidePoints = document.querySelectorAll(".dot"),
+            slideParent = document.querySelector(".portfolio-content");
+        
+        let currentSlide = 0;
+        let slideInterval;
+        // auto-play
+        const toggleActiveElement = (element, strClass) => element.classList.toggle(strClass);
+        
+        const autoPlaySlide = () => {
+            // Отключаем текущий слайд
+            toggleActiveElement(slideCollection[currentSlide], "portfolio-item-active");
+            toggleActiveElement(slidePoints[currentSlide], "dot-active");
+
+            currentSlide++;
+            if(currentSlide >= slideCollection.length) {
+                currentSlide = 0;
+            }
+            // Переключаем на новый слайд
+            toggleActiveElement(slideCollection[currentSlide], "portfolio-item-active");
+            toggleActiveElement(slidePoints[currentSlide], "dot-active");
+        };
+        const startSlider = (time=1500) => slideInterval = setInterval(autoPlaySlide, 1500);
+        const stopSlider = () => clearInterval(slideInterval);
+
+        slideParent.addEventListener("click", event => {
+            event.preventDefault();
+            const target = event.target;
+
+            if(!target.matches(".portfolio-btn, .dot")) {
+                return;
+            }
+
+            // Отключаем текущий слайд
+            toggleActiveElement(slideCollection[currentSlide], "portfolio-item-active");
+            toggleActiveElement(slidePoints[currentSlide], "dot-active");
+
+            if(target.matches("#arrow-right")) {
+                currentSlide++;
+            } else  if(target.matches("#arrow-left")) {
+                currentSlide--;
+            } else if(target.matches(".dot")) {
+                currentSlide = [...target.parentNode.children].indexOf(target);
+            }
+
+            if(currentSlide >= slideCollection.length) {
+                currentSlide = 0;
+            } else if(currentSlide < 0) {
+                currentSlide = slideCollection.length - 1;
+            }
+            // Переключаем на новый слайд
+            toggleActiveElement(slideCollection[currentSlide], "portfolio-item-active");
+            toggleActiveElement(slidePoints[currentSlide], "dot-active");
+        });
+        slideParent.addEventListener("mouseover", event => {
+            if(event.target.matches(".portfolio-btn, .dot")) {
+                stopSlider();
+            }
+
+
+        });
+        slideParent.addEventListener("mouseout", event => {
+            if(event.target.matches(".portfolio-btn, .dot")) {
+                startSlider();
+            }
+        });
+
+        startSlider(1500);
+
+    };
+    slideHandler();
 });
 
 
